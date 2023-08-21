fc701a3207f29163b13a057aa9b89c4c22d5091b
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-08-21 20:57:56 +0200

    Added drawable images for buttons

--- app/src/main/java/com/example/opsc7312icetask2pet/PetInteractionFragment.kt
+++ app/src/main/java/com/example/opsc7312icetask2pet/PetInteractionFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 
 class PetInteractionFragment : Fragment() {
@@ -18,6 +18,9 @@ class PetInteractionFragment : Fragment() {
     ): View? {
 
 
+        //val Interactionimage: ImageView = petinteraction.findViewById(R.id.Interactionimage)
+        //Interactionimage.setImageResource(android.R.drawable.resource_default)
+
         val petinteraction = inflater.inflate(R.layout.fragment_pet_interaction, container, false)
         val txtcurrenthunger: TextView = petinteraction.findViewById(R.id.txtcurrenthunger)
         val txtcurrentplay: TextView = petinteraction.findViewById(R.id.txtcurrentplay)
@@ -28,18 +31,23 @@ class PetInteractionFragment : Fragment() {
         txtcurrenthunger.text = pet.hunger.toString()
         txtcurrentplay.text = pet.happiness.toString()
         txtcurrentclean.text = pet.cleanliness.toString()
+        val Interactionimage: ImageView = petinteraction.findViewById(R.id.Interactionimage)
+        //Interactionimage.setImageResource(android.R.drawable.resource_default)
 
         btnfeed.setOnClickListener(){
             pet.hunger++
              txtcurrenthunger.text = pet.hunger.toString()
+            Interactionimage.setImageResource(R.drawable.feed)
         }
         btnplay.setOnClickListener(){
             pet.happiness++
             txtcurrentplay.text = pet.happiness.toString()
+            Interactionimage.setImageResource(R.drawable.play)
         }
         btnclean.setOnClickListener(){
             pet.cleanliness++
             txtcurrentclean.text = pet.cleanliness.toString()
+            Interactionimage.setImageResource(R.drawable.clean)
         }
         return petinteraction
     }
--- app/src/main/java/com/example/opsc7312icetask2pet/PetStatusFragment.kt
+++ app/src/main/java/com/example/opsc7312icetask2pet/PetStatusFragment.kt
@@ -1,11 +1,14 @@
 package com.example.opsc7312icetask2pet
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+
 
 val pet = Pet("Polly");
 class PetStatusFragment : Fragment() {
@@ -16,11 +19,16 @@ class PetStatusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        //val statusimage: ImageView = findViewById(R.id.statusimage)
+        //statusimage.setImageResource(R.drawable.resource_default)
         val petStatus = inflater.inflate(R.layout.fragment_pet_status, container, false)
         val name: TextView = petStatus.findViewById(R.id.textViewName)
-        val hunger: TextView =petStatus.findViewById(R.id.textViewHunger)
+        val hunger: TextView = petStatus.findViewById(R.id.textViewHunger)
         val happy: TextView = petStatus.findViewById(R.id.textViewHapp)
         val clean: TextView = petStatus.findViewById(R.id.textViewClean)
+
+        val statusimage: ImageView = petStatus.findViewById(R.id.Statusimage)
+        //statusimage.setImageResource(R.drawable.resource_default)
 
         name.text = pet.name
         hunger.text = pet.hunger.toString()
